3b7b617212881784b0c7f9dbd6d12b30f5b3f9bf
Owen Graham <owenxgraham@gmail.com>
2015-07-17 11:07:54 -0400

    Corrected image file paths, restructured nav, removed old JS code and added JQuery CDN to index.html

--- hilltop.js
+++ hilltop.js
@@ -4,31 +4,5 @@
 
 //Steps
 //1. attach click handler to nav elements
-//2. select the correct div
-//3. toggle div's display quality to on
-//4. make sure all other divs are off
-
-//Function runs with .onchange (see html, navmenu select)
-
-var changeMenu = function() {
-	//identify user choice
-var selector = document.getElementById("menus");
-var userChoice = selector.value;
-console.log(userChoice);
-
-//select correct div and toggle on
-if (userChoice === "foodmenu") {
-	var chosenDiv = document.getElementById("foodmenu");
-			console.log("Time to display")
-			chosenDiv.style.display = 'block';		
-	} else if (userChoice === "boozemenu") {
-	var chosenDiv = document.getElementById("boozemenu");
-			console.log("Time to display")
-			chosenDiv.style.display = 'block';		
-	};
-
-var defaultDiv = document.getElementById("default-content");
-	defaultDiv.style.display = 'none';
-
-
-};
+//2. select the right content
+//3. display as overlay/lightbox type window
